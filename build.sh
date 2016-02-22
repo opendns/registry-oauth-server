406f7dc46dca30c0b252487d6ffe3b5af7575b49
Jack Zheng <jackzhen@opendns.com>
2016-02-22 14:28:14 -0800

    generate certs

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#create certificate used by registry
+mkdir -p certs
+if [ ! -f cert/server.key ] & [ ! -f certs/server.crt ]; then
+    openssl req -subj '/CN=localhost/O=Registry Demo/C=US' -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout certs/server.key -out certs/server.crt
+fi
+
+
 if [ "$(uname)" == "Darwin" ]; then
     docker_ip="$(docker-machine ip default)"
 else
